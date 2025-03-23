26f5faecd6b18ddb7476e41deb61bd1c1430aceb
Nick <nickoszouros2@gmail.com>
2025-03-23 11:38:06 +0200

    I created a swap.c file

---
+++ swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int x = 28;
+    int y = 7;
+
+    printd("x is %i\n", x);
+    printd("y is %i\n", y);
+
+    // swapping...
+    int temp = x;
+    x = y;
+    y = temp;
+
+    printf("x is now %i\n", x);
+    printf("y is now %i\n", y);
+}
